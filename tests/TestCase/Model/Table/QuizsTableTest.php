243f3e95a819676cd95467e4c34d36a4e3e5d5df
Phuong Trung <trung.ta@spc-vn.com>
2016-12-23 09:42:51 +0700

    update moi

---
+++ tests/TestCase/Model/Table/QuizsTableTest.php
@@ -0,0 +1,89 @@
+<?php
+namespace App\Test\TestCase\Model\Table;
+
+use App\Model\Table\QuizsTable;
+use Cake\ORM\TableRegistry;
+use Cake\TestSuite\TestCase;
+
+/**
+ * App\Model\Table\QuizsTable Test Case
+ */
+class QuizsTableTest extends TestCase
+{
+
+    /**
+     * Test subject
+     *
+     * @var \App\Model\Table\QuizsTable
+     */
+    public $Quizs;
+
+    /**
+     * Fixtures
+     *
+     * @var array
+     */
+    public $fixtures = [
+        'app.quizs',
+        'app.candidates',
+        'app.users',
+        'app.quiz_details',
+        'app.quizzes',
+        'app.questions',
+        'app.answers'
+    ];
+
+    /**
+     * setUp method
+     *
+     * @return void
+     */
+    public function setUp()
+    {
+        parent::setUp();
+        $config = TableRegistry::exists('Quizs') ? [] : ['className' => 'App\Model\Table\QuizsTable'];
+        $this->Quizs = TableRegistry::get('Quizs', $config);
+    }
+
+    /**
+     * tearDown method
+     *
+     * @return void
+     */
+    public function tearDown()
+    {
+        unset($this->Quizs);
+
+        parent::tearDown();
+    }
+
+    /**
+     * Test initialize method
+     *
+     * @return void
+     */
+    public function testInitialize()
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+
+    /**
+     * Test validationDefault method
+     *
+     * @return void
+     */
+    public function testValidationDefault()
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+
+    /**
+     * Test buildRules method
+     *
+     * @return void
+     */
+    public function testBuildRules()
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+}

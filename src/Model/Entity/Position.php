60c28d2431938c7c28e46e91ed0ed7c168c806d8
Phuong Trung <trung.ta@spc-vn.com>
2017-01-23 08:54:35 +0700

    update user page (add file)

---
+++ src/Model/Entity/Position.php
@@ -0,0 +1,16 @@
+<?php
+namespace App\Model\Entity;
+
+use Cake\ORM\Entity;
+
+/**
+ * Position Entity
+ *
+ * @property int $id
+ * @property string $name
+ * @property int $is_delete
+ */
+class Position extends Entity
+{
+
+}
